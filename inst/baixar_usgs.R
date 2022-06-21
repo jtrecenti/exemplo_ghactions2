3d09b490a42252098b44d262b07187d6a6aef13c
Julio Trecenti <julio.trecenti@gmail.com>
2022-06-21 20:16:52 -0300

    colocar na estrutura de pacotes

--- inst/baixar_usgs.R
+++ inst/baixar_usgs.R
@@ -1,8 +1,8 @@
-install.packages(
-  "httr",
-  repos = "https://packagemanager.rstudio.com/all/latest"
-)
+# install.packages(
+#   "httr",
+#   repos = "https://packagemanager.rstudio.com/all/latest"
+# )
 
 u_usgs <- "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
-arquivo <- "usgs_r.json"
+arquivo <- "data-raw/usgs_r.json"
 httr::GET(u_usgs, httr::write_disk(arquivo, overwrite = TRUE))
